713ace58377a3ef8fd8713a75d431106f1eb4d8c
Alexander Efimov <efimokthefatass@gmail.com>
2023-09-01 13:15:59 +0300

    fix translator

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -1,45 +1,37 @@
 const { derived, writable } = require('svelte/store');
+const { capitalize } = require('./others');
 
 class Translator {
   constructor() {
+    this.fallbackLng = 'en';
     this.dictionary = {};
-    this.stores = {};
     this.locale = writable();
-    this.lng = '';
+    this.lng = this.fallbackLng;
     this.__ = derived(this.locale, () => this.t.bind(this));
   }
   addMessages(lng, dic) {
     this.dictionary[lng] = dic;
   }
   setLocale(lng) {
-    this._checkLocaleExists(lng);
-    this.lng = lng;
-    this.locale.set(lng);
+    if (!this.dictionary[lng]) {
+      console.warn(`No translations found for ${lng}!`);
+      this.lng = this.fallbackLng;
+    } else {
+      this.lng = lng;
+    }
+    this.locale.set(this.lng);
   }
-  changeLocale(newLocale) {
-    this._checkLocaleExists(newLocale);
-    this.locale.set(newLocale);
-    this.lng = newLocale;
-  }
-  t(key) {
-    return (
-      key
-        .split('.')
-        .reduce(
-          (dictionary, key) => dictionary[key],
-          this.dictionary[this.lng]
-        ) || key
-    );
-  }
-  _checkLocaleExists(lng) {
-    if (!this.dictionary[lng])
-      throw new Error(`No translations loaded for ${lng}!`);
+  t(key, noCapitalize) {
+    if (!key) return;
+    const translation = this.dictionary[this.lng][key] || key;
+    return noCapitalize ? translation : capitalize(translation);
   }
 }
 
 const translator = new Translator();
 translator.addMessages('ru', require('../../locale/ru.json'));
 translator.addMessages('en', require('../../locale/en.json'));
+// translator.setLocale('ru');
 translator.setLocale(process.env.LANG.slice(0, 2));
 
 module.exports = translator;
